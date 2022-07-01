8d79498ab511fa20a71ec7bd85025b41499a0adb
HBK <sammcowen@gmail.com>
2022-07-01 13:02:08 -0700

    fixed icons for floating divs in intro page

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Intro.css';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
 import Vector1 from '../../img/Vector1.png';
 import Vector2 from '../../img/Vector2.png';
 import Samm from '../../img/samm1.png';
-import FloatingDiv from '../FloatingDiv/FloatingDiv';
+
 import thumbup from '../../img/thumbup.png';
-import Crown from '../../img/crown.png';
+import crown from '../../img/crown.png';
 // import glassesemoji from '../../img/glassesemoji.png';
 
 
@@ -32,7 +33,7 @@ const Intro = () => {
                 <img src={Vector2} alt="" />
                 <img src={Samm} alt="" />
                 <div style={{top:'-4%',left: '68%'}}>
-                    <FloatingDiv image={Crown}txt1='Web'txt2='Developer'/>
+                    <FloatingDiv image={crown}txt1='Web'txt2='Developer'/>
                 </div>
                 <div style={{}}>
                     <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award' />
