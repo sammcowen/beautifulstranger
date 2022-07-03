840f81de753abfa6bbeeb69a16b1ab3119465f21
HBK <sammcowen@gmail.com>
2022-07-03 12:55:23 -0700

    made button functioning to download resume onclick

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import './Services.css';
 import HeartEmoji from '../../img/heartemoji.png';
 import Glasses from '../../img/glasses.png';
-// import Humble from '../../img/humble.png';
+import Humble from '../../img/humble.png';
 import Card from '../Card/Card';
+import Resume from './resume.pdf'
 const Services = () => {
     return (
         <div className="services">
@@ -14,7 +15,8 @@ const Services = () => {
                 <span>Services</span>
                 <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum <br/>
                  ipsum quas quae tenetur eos aliquam dolor id a</span>
-                    <button className='button s-button'>Download CV</button>
+                 <a href={Resume} download>
+                    <button className='button s-button'>Download CV</button></a>
                     <div className="blur s-blur1" style={{ background:'#ABF1FF94'}}></div>
 
             </div>
@@ -28,6 +30,13 @@ const Services = () => {
                     <Card emoji = {Glasses} heading = {'Full Stack Dev'} detail = {'MongoDb, Express, React, Node.js'}/>
                     
                  </div>
+                 {/* 3rd card */}
+                 <div style ={{top:'19rem', left:'12rem'}}> <Card
+                 emoji={Humble}
+                 heading={'Idk'}
+                 detail = {
+                    'lorem ipsum dummy text are usually use in section bla bla '
+                 }/></div>
             </div>
         </div>
     )
